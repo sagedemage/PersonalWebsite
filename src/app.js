0d9ceeb1ff78024943094aa2247e1ee27e26eaf9
Salmaan Saeed <salsaeed999@gmail.com>
2022-06-05 16:45:44 -0400

    added newline between navbar and simple webpage

--- src/app.js
+++ src/app.js
@@ -1,16 +1,26 @@
 
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Link} from "react-router-dom";
+
 import { Home } from "./components/home"
 import { About } from "./components/about"
 
 
 function App() {
 return (
+    <>
+        <nav>
+            <Link to="/"> Home </Link> | {" "}
+            <Link to="about"> About </Link>
+        </nav>
+
+        <br />
+
         <Routes>
             <Route path='/' element={<Home />}> </Route>
             <Route path='about' element={<About />}> </Route>
         </Routes>
+    </>
     ) 
 }
 
