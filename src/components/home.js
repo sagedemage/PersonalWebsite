227f53eb7fcc1ae6bd7bcb3e7179ba31658244b9
Salmaan Saeed <salsaeed999@gmail.com>
2022-06-06 03:53:34 -0400

    added more content to the home page.

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,16 @@
 
+import profile from '../assets/images/salmaan_saeed.webp';
+
+import "./home.css"
+
 export const Home = () => {
-    return ( 
-    <div> 
-        <h2> About Me </h2>
-		<p> 
-			Hi, my name is Salmaan Saeed. I'm majoring in Information Technology.
-			I expect to graduate in December 2023. I'm interested in getting into
+    return (
+    <div id="home-content"> 
+		<img id="profile" src={profile} alt="desktop front view" />
+		<h2> About Me </h2>
+		<p>
+			Hi, my name is Salmaan Saeed. I'm a student majoring in Information Technology
+			at New Jersey Institute of Technology. I expect to graduate in December 2023. I'm interested in getting into
 			web development and system administration.
 		</p>
 				
@@ -13,9 +18,24 @@ export const Home = () => {
 		<p> 
 			During my free time, I like to play video games and watch anime. My favorite video games 
 			are Terraria and Cave Story. My favorite anime series are Samurai Champloo, Clannad, JoJo's Bizarre Adventure, 
-			and Hunter x Hunter. 
+			and Hunter X Hunter. 
 		</p>
+		<h3> Personal Projects </h3>
+		<p class="project-title"> Personal Website </p>
+		<ul class="tech">
+			<li> Framework: React </li>
+			<li> Languages: JavaScript and HTML/CSS </li>
+			<li> Repo: <a href="https://github.com/sagedemage/PersonalWebsiteReact"> PersonalWebsiteReact </a>
+			</li>
+		</ul>
+		<p class="project-title"> Discussion Forum </p>
+		<ul class="tech">
+			<li> Framework: Flask </li>
+			<li> Languages: Python, JavaScript and HTML/CSS </li>
+			<li> Website: <a href="https://final-flask-start-prod.herokuapp.com"> Discussion Forum </a> </li>
+			<li> Repo: <a href="https://github.com/sagedemage/final_flask_start"> final_flask_start </a>
+			</li>
+		</ul>
     </div>
-     
     );
 }
