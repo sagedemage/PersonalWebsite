1a949d413a6d22c9f587ff533ac8dc20ec7f164d
Sage <salsaeed999@gmail.com>
2022-12-16 00:15:54 -0500

    fixed page height when page is empty

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,21 +9,18 @@ import { MyNavBar } from 'components/navigation_bar/navigation_bar';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<div>
+		<div id="body">
 			{/* Navigation Bar */}
 			<MyNavBar></MyNavBar>
-
-			<div id="page">
-				<div id="content">
-					<Component {...pageProps} />
-				</div>
-
-				<footer className="footer">
-        			<div id="copyright">
-						<p id="copyright"> &copy; {(new Date().getFullYear())} Salmaan Saeed </p>
-					</div>
-      			</footer>
+			<div id="content">
+				<Component {...pageProps} />
 			</div>
+
+			<footer className="footer">
+				<div id="copyright">
+					<p id="copyright"> &copy; {(new Date().getFullYear())} Salmaan Saeed </p>
+				</div>
+			</footer>
 		</div>
 		);
 }
