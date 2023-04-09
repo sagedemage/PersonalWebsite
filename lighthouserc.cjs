5687d6e7acf86fedfd7b6ed5758a6fa6be7beadc
Salmaan Saeed <salsaeed999@gmail.com>
2023-04-09 18:05:40 -0400

    lighthouse workflow asserts if it meets the lighthouse recommended requirements

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -1,14 +1,17 @@
 module.exports = {
-    ci: {
-        collect: {
-            url: [
-              'http://localhost:3000/',
-              'http://localhost:3000/projects'
-            ],
-            startServerCommand: 'npm run build && npm run start',
-          },
-      upload: {
-        target: 'temporary-public-storage',
-      },
+  ci: {
+    collect: {
+      url: [
+        'http://localhost:3000/',
+        'http://localhost:3000/projects'
+      ],
+      startServerCommand: 'npm run build && npm run start',
     },
-  };
+    assert: {
+      preset: 'lighthouse:recommended',
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
