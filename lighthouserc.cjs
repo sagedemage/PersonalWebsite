1a5f7a77d72f90057119693021fad0bd36d10605
Salmaan Saeed <salsaeed999@gmail.com>
2023-04-09 20:21:18 -0400

    specify lighthouse assertions in the lighthouse config

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -8,7 +8,12 @@ module.exports = {
       startServerCommand: 'npm run build && npm run start',
     },
     assert: {
-      preset: 'lighthouse:recommended',
+      assertions: {
+        "categories:performance": ["error", {"minScore": 0.75}],
+        "categories:accessibility": ["error", {"minScore": 0.9}],
+        "categories:best-practices": ["error", {"minScore": 0.9}],
+        "categories:seo": ["error", {"minScore": 0.9}],
+      },
     },
     upload: {
       target: 'temporary-public-storage',
