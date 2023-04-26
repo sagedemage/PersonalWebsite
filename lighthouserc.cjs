d4f3dd07e88382ba78d527187fad6c5beebaaa2f
Salmaan Saeed <salsaeed999@gmail.com>
2023-04-26 03:52:37 -0400

    increased the performance assertion for lighthouse

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -9,7 +9,7 @@ module.exports = {
     },
     assert: {
       assertions: {
-        "categories:performance": ["error", {"minScore": 0.74}],
+        "categories:performance": ["error", {"minScore": 0.9}],
         "categories:accessibility": ["error", {"minScore": 0.9}],
         "categories:best-practices": ["error", {"minScore": 0.9}],
         "categories:seo": ["error", {"minScore": 0.9}],
