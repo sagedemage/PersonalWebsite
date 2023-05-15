9348b28d233c6db82ca1ae516034bc7751da59d7
Salmaan Saeed <salsaeed999@gmail.com>
2023-05-15 14:37:25 -0400

    use node adapter over netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,19 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import compress from "astro-compress";
 
-// https://astro.build/config
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 
 // https://astro.build/config
+import node from "@astrojs/node";
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [react(), compress()],
   output: "server",
-  adapter: netlify()
+  adapter: node({
+    mode: "standalone"
+  })
 });
--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -5,14 +5,14 @@ module.exports = {
         'http://localhost:3000/',
         'http://localhost:3000/projects'
       ],
-      startServerCommand: 'npm run build && npm run start',
+      startServerCommand: 'npm run build && npm run preview',
     },
     assert: {
       assertions: {
-        "categories:performance": ["error", {"minScore": 0.9}],
-        "categories:accessibility": ["error", {"minScore": 0.9}],
-        "categories:best-practices": ["error", {"minScore": 0.9}],
-        "categories:seo": ["error", {"minScore": 0.9}],
+        "categories:performance": ["error", {"minScore": 0.95}],
+        "categories:accessibility": ["error", {"minScore": 0.95}],
+        "categories:best-practices": ["error", {"minScore": 0.95}],
+        "categories:seo": ["error", {"minScore": 0.95}],
       },
     },
     upload: {
