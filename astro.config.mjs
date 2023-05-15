a530d4e757b5c58e1b5d66affbf3fa290e4b3957
Salmaan Saeed <salsaeed999@gmail.com>
2023-05-15 15:40:30 -0400

    removed @astrojs/netlify package

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,6 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import compress from "astro-compress";
 
-import netlify from "@astrojs/netlify/functions";
-
-// https://astro.build/config
 
 // https://astro.build/config
 import node from "@astrojs/node";
