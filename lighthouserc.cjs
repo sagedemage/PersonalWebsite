06001d0264ac22602902276a9dc223c871b6d4fa
Salmaan Saeed <salsaeed999@gmail.com>
2023-05-15 20:04:05 -0400

    lower seo requirement to 90%

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -5,7 +5,7 @@ module.exports = {
         'http://localhost:3000/',
         'http://localhost:3000/projects'
       ],
-      startServerCommand: 'npm run build && npm run preview',
+      startServerCommand: 'npm run dev',
     },
     assert: {
       assertions: {
@@ -13,7 +13,7 @@ module.exports = {
         "categories:performance": ["error", {"minScore": 0.9}],
         "categories:accessibility": ["error", {"minScore": 1.0}],
         "categories:best-practices": ["error", {"minScore": 1.0}],
-        "categories:seo": ["error", {"minScore": 1.0}],
+        "categories:seo": ["error", {"minScore": 0.9}],
         
         // Warnings
         "categories:performance": ["warn", {"minScore": 0.95}],
