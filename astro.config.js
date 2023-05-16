0e865bdad023b1abb7545721d107b7c657e827ea
Salmaan Saeed <salsaeed999@gmail.com>
2023-05-15 20:06:14 -0400

    used netlify adapter for it to deploy on netlify

--- astro.config.js
+++ astro.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import compress from "astro-compress";
-
 import node from "@astrojs/node";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), compress()],
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: netlify()
 });
