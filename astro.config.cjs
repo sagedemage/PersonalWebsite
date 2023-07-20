c6a5e7eb6fffd8ae8bbde8b21dd0395fb02d5b2a
Salmaan Saeed <salsaeed999@gmail.com>
2023-07-20 18:36:07 -0400

    installed tailwind

--- astro.config.cjs
+++ astro.config.cjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import compress from "astro-compress";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), compress()],
+  integrations: [react(), compress(), tailwind()],
   site: 'https://sagedemage.github.io',
-  base: '/PersonalWebsite',
+  base: '/PersonalWebsite'
 });
