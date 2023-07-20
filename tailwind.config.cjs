db06da99d57a73c60f7809877f564aa23116a518
Salmaan Saeed <salsaeed999@gmail.com>
2023-07-20 19:40:27 -0400

    improved the appearance of the web pages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,16 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
 		extend: {},
 	},
 	plugins: [],
