84d2cda7b765d1b9e33b1c48db9d78240c9db527
Salmaan Saeed <salsaeed999@gmail.com>
2023-07-21 15:04:14 -0400

    installed and configured daisy ui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
+	daisyui: {
+    darkTheme: "light",
+  },
+  theme: {
     screens: {
       sm: '480px',
       md: '768px',
       lg: '976px',
       xl: '1440px',
+    },
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': 'SeaGreen',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
     },
     fontFamily: {
       sans: ['Graphik', 'sans-serif'],
@@ -14,5 +28,5 @@ module.exports = {
     },
 		extend: {},
 	},
-	plugins: [],
+	plugins: [require("daisyui")],
 }
