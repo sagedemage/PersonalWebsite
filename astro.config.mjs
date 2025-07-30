97696890b1d918db4c671a490dd432ab8284ec2e
Salmaan Saeed <salsaeed999@gmail.com>
2025-07-30 14:38:43 -0400

    removed dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import betterImageService from 'astro-better-image-service';
-import compressor from 'astro-compressor';
 import playformCompress from '@playform/compress';
-import mdx from '@astrojs/mdx';
-import remarkToc from 'remark-toc';
-import rehypePresetMinify from 'rehype-preset-minify';
-
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    betterImageService(),
-    compressor(),
     playformCompress(),
-    mdx(
-      {
-        syntaxHighlight: 'shiki',
-        shikiConfig: { theme: 'dracula' },
-        remarkPlugins: [remarkToc],
-        rehypePlugins: [rehypePresetMinify],
-        remarkRehype: { footnoteLabel: 'Footnotes' },
-        gfm: false,
-      }
-    )
   ],
   site: 'https://sagedemage.github.io',
   base: '/PersonalWebsite',
