89096417c5045fc90b65c17562ab198b8ecc8295
Salmaan Saeed <salsaeed999@gmail.com>
2026-05-04 19:36:55 -0400

    formatted and linted the codebase

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,73 +1,75 @@
 <script setup>
 import linkedin from "../assets/icons/linkedin.webp";
 import github from "../assets/icons/github.webp";
-import { onMounted, ref } from 'vue';
+import { ref } from "vue";
 
-const nav_menu_display = ref("none")
+const nav_menu_display = ref("none");
 
 const open_and_close_nav_menu = () => {
-  console.log(nav_menu_display.value)
   if (nav_menu_display.value === "none") {
-    nav_menu_display.value = "block"
+    nav_menu_display.value = "block";
   } else if (nav_menu_display.value === "block") {
-    nav_menu_display.value = "none"
+    nav_menu_display.value = "none";
   }
-}
+};
 </script>
 
 <template>
-<!-- Navbar -->
-<div class="navbar bg-base-100 shadow-sm bg-cornflower-blue fixed">
-  <div class="navbar-left">
-    <div class="dropdown inline-block">
-      <button
-        class="dropdown-btn px-[14px] py-[8px] btn rounded-full hover:bg-gray-200 bg-transparent border-transparent shadow-none hover:shadow-sm shadow-white"
-        aria-label="Dropdown Button" @click="open_and_close_nav_menu"
-      >
-        <i class="dropdown-btn fa fa-bars"></i>
-      </button>
-      <div
-        id="dropdown-menu"
-        class="dropdown-content bg-cornflower-blue rounded-md shadow-sm"
-        :style="{display: nav_menu_display}"
-      >
-        <a class="!text-gray-900" href="/PersonalWebsite"> Home </a>
-        <a class="!text-gray-900" href="/PersonalWebsite/projects">
-          Projects
-        </a>
+  <!-- Navbar -->
+  <div class="navbar bg-base-100 shadow-sm bg-cornflower-blue fixed">
+    <div class="navbar-left">
+      <div class="dropdown inline-block">
+        <button
+          class="dropdown-btn px-[14px] py-[8px] btn rounded-full hover:bg-gray-200 bg-transparent border-transparent shadow-none hover:shadow-sm shadow-white"
+          aria-label="Dropdown Button"
+          @click="open_and_close_nav_menu"
+        >
+          <i class="dropdown-btn fa fa-bars"></i>
+        </button>
+        <div
+          id="dropdown-menu"
+          class="dropdown-content bg-cornflower-blue rounded-md shadow-sm"
+          :style="{ display: nav_menu_display }"
+        >
+          <a class="!text-gray-900" href="/PersonalWebsite"> Home </a>
+          <a class="!text-gray-900" href="/PersonalWebsite/projects">
+            Projects
+          </a>
+        </div>
       </div>
+      <a
+        class="!text-black font-bold text-xl btn rounded-md hover:bg-gray-200 bg-transparent border-transparent shadow-none hover:shadow-sm shadow-white p-2"
+        href="/PersonalWebsite"
+      >
+        Salmaan Saeed
+      </a>
     </div>
-    <a
-      class="!text-black font-bold text-xl btn rounded-md hover:bg-gray-200 bg-transparent border-transparent shadow-none hover:shadow-sm shadow-white p-2"
-      href="/PersonalWebsite"
-    >
-      Salmaan Saeed
-    </a>
-  </div>
-  <div class="navbar-center"></div>
-  <div class="navbar-right">
-    <a target="_blank" href="https://github.com/sagedemage">
-      <img class="ml-0.5 mr-0.5 float-left" 
-        :src="github.src"
-        alt="my github profile"
-        width="35"
-        height="35"
+    <div class="navbar-center"></div>
+    <div class="navbar-right">
+      <a target="_blank" href="https://github.com/sagedemage">
+        <img
+          class="ml-0.5 mr-0.5 float-left"
+          :src="github.src"
+          alt="my github profile"
+          width="35"
+          height="35"
         />
-    </a>
+      </a>
 
-    <a
-      target="_blank"
-      href="https://www.linkedin.com/in/salmaan-saeed-6a5582197/"
-    >
-      <img class="ml-0.5 mr-0.5 float-left"
-        :src="linkedin.src"
-        alt="my linkedin profile"
-        width="35"
-        height="35"
+      <a
+        target="_blank"
+        href="https://www.linkedin.com/in/salmaan-saeed-6a5582197/"
+      >
+        <img
+          class="ml-0.5 mr-0.5 float-left"
+          :src="linkedin.src"
+          alt="my linkedin profile"
+          width="35"
+          height="35"
         />
-    </a>
+      </a>
+    </div>
   </div>
-</div>
 </template>
 
 <style>
