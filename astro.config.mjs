7eba6ae6746b8b2c893033ab9cbf8b1169a28e43
Salmaan Saeed <salsaeed999@gmail.com>
2026-05-04 19:18:16 -0400

    used Vue.js with Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import playformCompress from "@playform/compress";
 
 import mdx from "@astrojs/mdx";
 
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [playformCompress(), mdx()],
+  integrations: [playformCompress(), mdx(), vue()],
   site: "https://sagedemage.github.io",
   base: "/PersonalWebsite",
   vite: {
