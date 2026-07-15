f4cc15361e41014c8ca47523b0bab7c75cc1d619
Salmaan Saeed <salsaeed999@gmail.com>
2026-07-15 18:01:00 -0400

    updated the astro framework to 7.0.9

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config";
 import { satteri } from "@astrojs/markdown-satteri";
 import solidJs from "@astrojs/solid-js";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [solidJs()],
@@ -11,6 +13,6 @@ export default defineConfig({
   site: "https://sagedemage.github.io",
   base: "/PersonalWebsite",
   vite: {
-    plugins: [],
+    plugins: [tailwindcss()],
   },
 });
